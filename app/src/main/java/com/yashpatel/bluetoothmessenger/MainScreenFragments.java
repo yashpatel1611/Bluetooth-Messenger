f74c2e8b7ced94994da64a60b515c0a102c41fcc
yashle1611 <yashpatel1611@gmail.com>
2020-02-01 21:21:05 +0000

    -Going to use ViewPager to navigate tabs in main screen activity -Renamed original MainActivity.java which handled sign in to LoginScreen.java -Created a new MainActivity.java which will handle main screen activity

---
+++ app/src/main/java/com/yashpatel/bluetoothmessenger/MainScreenFragments.java
@@ -0,0 +1,13 @@
+package com.yashpatel.bluetoothmessenger;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainScreenFragments extends Fragment {
+
+    PagerAdapter pagerAdapter;
+    ViewPager viewPager;
+
+
+}

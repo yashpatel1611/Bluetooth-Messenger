7804af12118045874db315da4a10c76072ec3683
yashle1611 <yashpatel1611@gmail.com>
2020-02-03 06:55:27 +0000

    -Made login screen look better -Added animations to login screen

--- app/src/main/java/com/yashpatel/bluetoothmessenger/LoginScreen.java
+++ app/src/main/java/com/yashpatel/bluetoothmessenger/LoginScreen.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
@@ -19,8 +22,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginScreen extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 9001;
-    TextView tv;
 
+    TextView loginScreenTV;
+    TextView signInPromptTV;
+    SignInButton signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,31 +38,38 @@ public class LoginScreen extends AppCompatActivity {
 
         final GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+        signInButton = findViewById(R.id.signInButton);
+        signInButton.setColorScheme(1);
+        signInButton.setVisibility(View.INVISIBLE);
+
+        loginScreenTV = findViewById(R.id.loginScreenTV);
+        signInPromptTV = findViewById(R.id.signInPromptTV);
+        signInPromptTV.setVisibility(View.INVISIBLE);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
-                    case (R.id.sign_in_button):
+                    case (R.id.signInButton):
                         signIn(mGoogleSignInClient);
                         break;
                 }
             }
-
         });
-    }
 
+        runAnimations();
+
+
+    }
 
     protected void onStart() {
         super.onStart();
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null) {
-            setContentView(R.layout.activity_main_screen);
-            //tv = findViewById(R.id.textView1);
-            //tv.setText(account.getDisplayName());
-
-        }
+//        if (account != null) {
+//            setContentView(R.layout.activity_main_screen);
+//
+//        }
     }
-
 
     private void signIn(GoogleSignInClient mGoogleSignInClient) {
         Intent intent = mGoogleSignInClient.getSignInIntent();
@@ -83,7 +95,7 @@ public class LoginScreen extends AppCompatActivity {
 
             // Signed in successfully, show authenticated UI.
             Log.e("account", account.getDisplayName() + account.getEmail() + account.getAccount());
-            setContentView(R.layout.activity_main_screen);
+//            setContentView(R.layout.activity_main_screen);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
@@ -92,5 +104,41 @@ public class LoginScreen extends AppCompatActivity {
         } catch (NullPointerException e) {
             Log.w("Error", e);
         }
+    }
+
+    private void runAnimations() {
+        final Animation textAnimation = AnimationUtils.loadAnimation(this, R.anim.welcome_text_view_animations);
+        textAnimation.reset();
+
+        final Animation buttonAnimation = AnimationUtils.loadAnimation(this, R.anim.sign_in_button_animations);
+        buttonAnimation.reset();
+
+        final Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.sign_in_promt_animations);
+        fadeInAnimation.reset();
+
+        final Animation translateAnimation = AnimationUtils.loadAnimation(this, R.anim.translate_x);
+
+        textAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                signInButton.setVisibility(View.VISIBLE);
+                signInPromptTV.setVisibility(View.VISIBLE);
+                signInButton.startAnimation(buttonAnimation);
+                signInPromptTV.startAnimation(fadeInAnimation);
+                loginScreenTV.startAnimation(translateAnimation);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        loginScreenTV.startAnimation(textAnimation);
     }
 }

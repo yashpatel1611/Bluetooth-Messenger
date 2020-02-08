96d9dfd9aa3a571be3bfb149bf529526d23d8cbc
yashle1611 <yashpatel1611@gmail.com>
2020-02-08 11:29:25 +0000

    - Added methods to TabPagerAdapter.java - Created 'Everyone' fragment - is auto-generated rn (need to change)

--- app/src/main/java/com/yashpatel/bluetoothmessenger/TabPagerAdapter.java
+++ app/src/main/java/com/yashpatel/bluetoothmessenger/TabPagerAdapter.java
@@ -1,7 +1,6 @@
 package com.yashpatel.bluetoothmessenger;
 
-import android.content.Context;
-
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -9,17 +8,53 @@ import androidx.fragment.app.FragmentPagerAdapter;
 public class TabPagerAdapter extends FragmentPagerAdapter {
 
 
-    public TabPagerAdapter(Context context, FragmentManager fm) {
+    public TabPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                fragment = new everyone_fragment();
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+        return fragment;
+
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        CharSequence title = null;
+
+        switch (position) {
+            case 0:
+                title = "Everyone";
+                break;
+            case 1:
+                title = "Nearby";
+                break;
+            case 2:
+                title = "Chats";
+                break;
+            case 3:
+                title = "Contacts";
+                break;
+        }
+        return title;
     }
 }
